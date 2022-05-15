f58bd5d92286aacc1ec207f6de03e1db810e9cdd
meindre1 <draebondes@gmail.com>
2022-05-15 16:24:32 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title : 'Cuaca',
+        name : 'MEINDRE-Blog'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title : 'About Me',
+        name : 'MEINDRE-Blog'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            message : 'You must to provde the address !'
+        })
+    }
+    geocode(req.query.address,(error,{latitude,longitude,location = {}})=>{
+        if(error){
+            return res.send({
+                error : error
+            })
+        }
+        forecast(longitude,latitude,(error,dataForecast)=>{
+            if(error){
+                return res.send({
+                    error : error
+                })
+            }
+            res.send({
+                address : req.query.address,
+                forecast : dataForecast,
+                location : location
+            })
+        })
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title : 'Help Page',
+        name : 'MEINDRE-Blog'
+    })
+})
+
+app.get('/article',(req,res)=>{
+    res.render('article',{
+        title : 'Artikel',
+        sub : 'Electrical Device',
+        name : 'MEINDRE-Blog'
+    })
+})
+
+app.get('/thermocouple',(req,res)=>{
+    res.render('thermocouple',{
+        title : 'Artikel',
+        sub : 'Thermocouple',
+        name : 'MEINDRE-Blog'
+        
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title : '404 Page',
+        name : 'MEINDRE-Blog'
+    })
+})
+
+app.listen(port,()=>{
+    console.log('Server is serv on ' + port)
+})
